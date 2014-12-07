84cff6c65ce565082190982f262b6462f4089d81
ShinDarth <borzifrancesco@gmail.com>
2014-12-07 13:25:08 +0100

    RectDraw.tcl

--- Average.tcl
+++ Average.tcl
@@ -1,9 +1,14 @@
+# Calculate the average
+
 puts "Insert the amount of numbers:"
 gets stdin n
+
 set sum 0
+
 for {set i 1} {$i <= $n} {incr i} {
     puts "Insert number $i:"
     gets stdin tmp
     incr sum $tmp
 }
+
 puts "The average is [expr $sum/$n]"
